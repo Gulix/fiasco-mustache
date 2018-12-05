e7f16e502afe61ad5bccc346b8d0caee8c52fd8f
Nicolas Ronvel <gulix33xp@yahoo.fr>
2018-12-05 18:39:30 +0100

    Added english file

---
+++ generator/languages/en.js
@@ -0,0 +1,12 @@
+var data = {
+  label: "English",
+  instasetup_title: "Insta-Setup",
+  instasetup_for3: "For 3 players ...",
+  instasetup_for4: "For 4 players, add ...",
+  instasetup_for34: "For 3 or 4 players ...",
+  instasetup_for45: "For 4 or 5 players, add ...",
+  instasetup_for5: "For 5 players, add ...",
+  instasetup_forany: "For any number of players ..."
+};
+
+langManager.addDictionary("en", data);
